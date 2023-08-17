5fdddcab16b05bbbd105fb2989448da969861e8e
todorpelivanov <95086224+todorpelivanov@users.noreply.github.com>
2023-08-17 13:09:37 +0200

    test_gitignore

---
+++ OpenScholar/OpenScholarApp/OpenScholarApp.Domain/Entities/TestCase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenScholarApp.Domain.Entities
+{
+    public class TestCase
+    {
+        public string TestUserName { get; set; } = string.Empty;
+        public string TestPassword { get; set; } = string.Empty;
+    }
+}
