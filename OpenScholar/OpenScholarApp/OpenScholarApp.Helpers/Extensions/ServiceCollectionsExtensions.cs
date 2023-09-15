871bc335bd183ad4d8ab65e9f4e8c5cbcfab68ae
todorpelivanov <95086224+todorpelivanov@users.noreply.github.com>
2023-09-15 02:23:12 +0200

    Ogranized Program.cs middleware for startup, made all services extendable and organized

--- OpenScholar/OpenScholarApp/OpenScholarApp.Helpers/Extensions/ServiceCollectionsExtensions.cs
+++ OpenScholar/OpenScholarApp/OpenScholarApp.Helpers/Extensions/ServiceCollectionsExtensions.cs
@@ -1,91 +1,110 @@
 ﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OpenScholarApp.Data.Context;
 using OpenScholarApp.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Swashbuckle.AspNetCore.Filters;
 using System.Text;
-using System.Threading.Tasks;
 
-//namespace OpenScholarApp.Helpers.Extensions
-//{
-    //public static class ServiceCollectionsExtensions
-    //{
-    //    public static MembershipBuilder AddDbContext(this IServiceCollection services, IConfiguration configuration)
-    //    {
-    //        var connectionString = configuration.GetConnectionString("DefaultConnection");
-    //        services.AddDbContext<OpenScholarDbContext>(options => options.UseSqlServer(connectionString)); // for MS sql database
-    //                                                                                                        //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString)); // for sqlite database
+namespace OpenScholarApp.Helpers.Extensions
+{
+    public static class ServiceCollectionsExtensions
+    {
 
-    //        return new(services, configuration);
-    //    }
-    //    public static MembershipBuilder AddIdentity(this MembershipBuilder builder)
-    //    {
-    //        builder.IdentityBuilder = builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
-    //            .AddEntityFrameworkStores<OpenScholarDbContext>()
-    //            .AddDefaultTokenProviders();
+        public class ConfigBuilder
+        {
+            public IServiceCollection Services { get; set; }
+            public IConfiguration Configuration { get; }
+            public IdentityBuilder IdentityBuilder { get; set; }
+            public AuthenticationBuilder AuthenticationBuilder { get; set; }
 
+            public ConfigBuilder(IServiceCollection services, IConfiguration configuration)
+            {
+                Services = services;
+                Configuration = configuration;
+            }
+        }
 
+        public static ConfigBuilder AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetSection("ConnectionString").Value;
+            services.AddDbContext<OpenScholarDbContext>(options =>
+                options.UseSqlServer(connectionString));
 
-    //        return builder;
-    //    }
-    //    public static MembershipBuilder AddAuthentication(this MembershipBuilder builder)
-    //    {
-    //        builder.AuthenticationBuilder = builder.Services.AddAuthentication(options =>
-    //        {
-    //            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    //            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    //            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    //        });
+            return new(services, configuration);
+        }
 
-    //        return builder;
-    //    }
-    //    public static MembershipBuilder AddJWT(this MembershipBuilder builder)
-    //    {
-    //        builder.AuthenticationBuilder.AddJwtBearer(options =>
-    //        {
-    //            options.SaveToken = true;
-    //            options.RequireHttpsMetadata = false;
-    //            options.TokenValidationParameters = new TokenValidationParameters()
-    //            {
-    //                ValidateIssuer = true,
-    //                ValidateAudience = true,
-    //                ValidAudience = builder.Configuration["JWT:ValidAudience"],
-    //                ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-    //                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
-    //            };
-    //        });
+        public static ConfigBuilder AddSwager(this ConfigBuilder builder)
+        {
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme, e.g" +
+                    "\"bearer {token} \"",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+            return builder;
+        }
 
-    //        return builder;
-    //    }
-        //public static MembershipBuilder AddBackgroundServices(this MembershipBuilder builder)
-        //{
-        //    builder.Services.AddCoursesBackgroundService();
-        //    return builder;
-        //}
-        //public static MembershipBuilder AddConfigurationOptions(this MembershipBuilder builder)
-        //{
-        //    builder.Services.Configure<MeterGramApiOptions>(builder.Configuration.GetSection("MeterGramApi"));
-        //    return builder;
-        //}
-    //}
+        public static ConfigBuilder AddIdentity(this ConfigBuilder builder)
+        {
+            builder.IdentityBuilder = builder.Services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+            }).AddEntityFrameworkStores<OpenScholarDbContext>()
+              .AddDefaultTokenProviders();
+            return builder;
+        }
 
-    //public class MembershipBuilder
-    //{
-    //    public IServiceCollection Services { get; set; }
-    //    public IConfiguration Configuration { get; }
-    //    public IdentityBuilder IdentityBuilder { get; set; }
-    //    public AuthenticationBuilder AuthenticationBuilder { get; set; }
+        public static ConfigBuilder AddCors(this ConfigBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("CORSPolicy", builder => builder.AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .SetIsOriginAllowed((hosts) => true));
+            });
+            return builder;
+        }
 
-    //    public MembershipBuilder(IServiceCollection services, IConfiguration configuration)
-    //    {
-    //        Services = services;
-    //        Configuration = configuration;
-    //    }
-    //}
-//}
-//}
+        public static ConfigBuilder AddAuthentication(this ConfigBuilder builder)
+        {
+            builder.AuthenticationBuilder = builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
+            return builder;
+        }
+
+        public static ConfigBuilder AddJWT(this ConfigBuilder builder, IConfiguration configuration)
+        {
+            var Token = configuration.GetSection("Token").Value;
+            builder.AuthenticationBuilder.AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Token)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+            return builder;
+        }
+    }
+}
--- OpenScholar/OpenScholarApp/OpenScholarApp/Program.cs
+++ OpenScholar/OpenScholarApp/OpenScholarApp/Program.cs
@@ -1,89 +1,28 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using OpenScholarApp.Data.Context;
-using OpenScholarApp.Domain.Entities;
 using OpenScholarApp.Helpers.DIContainer;
+using OpenScholarApp.Helpers.Extensions;
 using OpenScholarApp.Mappers.MapperConfig;
 using OpenScholarApp.Shared.Settings;
-using Swashbuckle.AspNetCore.Filters;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var appSettings = builder.Configuration.GetSection("AppSettings");
-builder.Services.Configure<AppSettings>(appSettings);
-AppSettings appSettingsObject = appSettings.Get<AppSettings>();
-
-var connectionString = builder.Configuration.GetConnectionString("ConnectionString");
-builder.Services.AddDbContext<OpenScholarDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddIdentityCore<ApplicationUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = true;
-})
-.AddEntityFrameworkStores<OpenScholarDbContext>()
-.AddDefaultTokenProviders();
-
-
-builder.Services.AddDbContext<OpenScholarDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("ConnectionString")));
-
+//builder.Services.Configure<AppSettings>(appSettings);
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
-    {
-        Description = "Standard Authorization header using the Bearer scheme, e.g" +
-        "\"bearer {token} \"",
-        In = ParameterLocation.Header,
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-    c.OperationFilter<SecurityRequirementsOperationFilter>();
-});
+builder.Services.AddDbContext(appSettings)
+    .AddAuthentication()
+    .AddJWT(appSettings)
+    .AddIdentity()
+    .AddCors()
+    .AddSwager();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.
-            GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CORSPolicy", builder => builder.AllowAnyMethod()
-    .AllowAnyHeader()
-    .AllowCredentials()
-    .SetIsOriginAllowed((hosts) => true));
-});
-
-//DependencyInjectionHelper.InjectDbContext(builder.Services, appSettingsObject.ConnectionString);
 DependencyInjectionHelper.InjectRepositories(builder.Services);
 DependencyInjectionHelper.InjectServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseAuthentication();
 app.UseAuthorization();
 
